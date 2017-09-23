59cbcaf2beff0ff5553c477623329e92256d4d2a
Sebastian Campoamor <sebascampoamor@gmail.com>
2017-09-23 18:14:58 -0300

    add nodejs and nginx, in docker

---
+++ appNode/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+const resources = require('./resources.json');
+const port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hi, Im root Node!');
+});
+
+app.use('/static', express.static(__dirname + '/static/files'));
+
+app.get('/resources/fast/:resource_id', function(req, res) {
+  const resource_id = req.params.resource_id;
+  res.send(resources[resource_id]);
+});
+
+app.get('/resources/slow/:resource_id', function(req, res) {
+  setTimeout(function(id){
+        res.send(resources[id]);
+    },
+    500,
+    req.params.resource_id );
+});
+
+app.listen(port, function () {
+  console.log('Node app listening on port http://localhost:' + port);
+});
